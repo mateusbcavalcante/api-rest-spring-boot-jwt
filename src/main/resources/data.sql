36d70363c18276c1ea72af263c09f38471c0935b
Mateus Bastos <mateus.bastos@softvaro.com.br>
2018-09-23 21:24:20 -0300

    configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,40 @@
+insert into user values(1,'1234','mateus.bastos');
+insert into user values(2,'1234','macos.andrade');
+insert into user values(3,'1234','eduardo.donato');
+insert into user values(4,'1234','joao.marcos');
+insert into user values(5,'1234','victor.vasconcelos');
+insert into user values(6,'1234','carlos.diego');
+insert into user values(7,'admin','admin');
+
+insert into user_movie values(1,424,1);
+insert into user_movie values(2,19404,1);
+insert into user_movie values(3,278,1);
+insert into user_movie values(4,238,1);
+insert into user_movie values(5,372058,1);
+insert into user_movie values(6,240,1);
+insert into user_movie values(7,497,1);
+insert into user_movie values(8,637,1);
+insert into user_movie values(9,538362,1);
+insert into user_movie values(10,680,1);
+
+insert into user_movie values(11,424,2);
+insert into user_movie values(12,19404,2);
+insert into user_movie values(13,278,2);
+insert into user_movie values(14,238,2);
+insert into user_movie values(15,372058,2);
+
+insert into user_movie values(16,19404,3);
+insert into user_movie values(17,278,3);
+insert into user_movie values(18,238,3);
+insert into user_movie values(19,372058,3);
+
+insert into user_movie values(20,19404,4);
+insert into user_movie values(21,278,4);
+insert into user_movie values(22,238,4);
+insert into user_movie values(23,372058,4);
+
+insert into user_movie values(24,637,5);
+insert into user_movie values(25,538362,5);
+insert into user_movie values(26,680,5);
+
+insert into user_movie values(27,680,7);
