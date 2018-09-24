7664c96687e5e49cf9cffaf2ff68adfd9d06f47a
Mateus Bastos <mateus.bastos@softvaro.com.br>
2018-09-23 21:37:39 -0300

    exception

---
+++ src/main/java/com/movie/api/exceptions/ApiException.java
@@ -0,0 +1,10 @@
+package com.movie.api.exceptions;
+
+public class ApiException extends RuntimeException {
+
+	private static final long serialVersionUID = -7979396198598876171L;
+
+	public ApiException(String s, Throwable throwable) {
+		super(s, throwable);
+	}
+}
